2294198c661a0a3cb2e9664e70f4d6168ff24739
Emre <degirmenci.a.emre@gmail.com>
2020-07-25 14:25:03 +0300

    Update PartialWordSearching.swift
    Updated because of your first version is not accepting `wordSearch("aB", ["za", "ab", "abc", "zab", "zbc"])` test case. So, it returns `[Empty]` instead of `["ab", "abc", "zab"]`

--- Codewars/7KYU/PartialWordSearching.swift
+++ Codewars/7KYU/PartialWordSearching.swift
@@ -10,6 +10,6 @@ import Foundation
 
 // https://www.codewars.com/kata/partial-word-searching/
 func wordSearch(_ str:String, _ arr:[String]) -> [String] {
-    let result = arr.filter({ $0.lowercased().contains(str) })
+    let result = arr.filter({ $0.lowercased().contains(str.lowercased()) })
     return result.isEmpty ? ["Empty"] : result
 }
